6d2f49bbf794ddd7a5049e229aaff9f5f0426fa0
mathfnz_macbook <matheus.fnds20@gmail.com>
2025-08-23 18:02:45 -0300

    add

---
+++ class02/try_except_if.py
@@ -0,0 +1,35 @@
+# %%
+# conversor de temperatura
+
+def calculate_celsius_fahrenheit(c: float) -> float:
+    return (c * 1.8) + 32
+
+def main() -> None:
+    try:
+        celsius: float = float(input("Type C temperature: "))
+        fahrenheit: float = calculate_celsius_fahrenheit(c=celsius)
+        print(f"{celsius} to fahrenheit: {fahrenheit:.2f}")
+    except ValueError:
+        print(f"Error: {ValueError}")
+    
+main()
+
+# %%
+# verificador de palíndromo
+
+def is_padridrome(p: str) -> bool:
+    x = p == p[::-1]
+    if x is True:
+        return True
+    else:
+        return False
+
+def main2() -> None:
+    
+    try:
+        palindromo: str = input("Type your word: ")
+        x: bool = is_padridrome(p=palindromo)
+        print({x})
+    except ValueError:
+        print(f"{ValueError}")
+main2()
