b08f3a8b8715d9c38a4a26dba8e6364b9ca3bcd7
mathfnz_macbook <matheus.fnds20@gmail.com>
2025-08-25 10:45:55 -0300

    using black

---
+++ class06/main.py
@@ -0,0 +1,31 @@
+# flake8 verifica com a pep
+
+# verificador de palíndromo
+
+
+def is_padridrome(p: str) -> bool:
+    """
+    Verify if the character is palindrome
+    Args:
+        str = the word choose to be verify
+    Return:
+        bool = True or False
+    """
+    x = p == p[::-1]
+    if x is True:
+        return True
+    else:
+        return False
+
+
+def main2() -> None:
+
+    try:
+        palindromo: str = input("Type your word: ")
+        x: bool = is_padridrome(p=palindromo)
+        print({x})
+    except ValueError:
+        print(f"{ValueError}")
+
+
+main2()
